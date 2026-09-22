3bb3e4970f8944c4d6493692c223a753825d2d18
b1rutes <140916364+b1rutes@users.noreply.github.com>
2026-09-22 11:28:05 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import smtplib
+from random import choice
+import datetime as dt
+
+def monday_email():
+    password = "vivs wjtf wmrc dnpe"
+    to_address = "williamsbirute@gmail.com"
+    senders_email = "williamsmugisha17@gmail.com"
+    now = dt.datetime.now()
+
+    if now.weekday() == 0:
+        with open("quotes.txt") as file:
+            quotes = file.readlines()
+            quote = choice(quotes)
+            message = f"Subject:Monday Quote\n\n{quote}"
+        senders_email = "williamsmugisha17@gmail.com"
+
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=senders_email, password=password)
+            connection.sendmail(
+                from_addr= senders_email,
+                to_addrs= to_address,
+                msg= message)
+
+
+if __name__ == "__main__":
+    monday_email()
+             
+    
+
+
